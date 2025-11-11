3777c81179f0a02ee8ab1aa208e57ee276fbde44
ALNoman78 <abdullaalno078@gmail.com>
2025-11-11 22:19:20 +0600

    data agg with empty opbject

--- data.js
+++ data.js
@@ -1,12 +1,48 @@
 const products = [
     { category: "Electronics", item: "Laptop", price: 1200, quantity: 1 },
     { category: "Electronics", item: "Smartphone", price: 900, quantity: 2 },
-    { category: "Home Appliances", item: "Microwave Oven", price: 300, quantity: 1 },
-    { category: "Furniture", item: "Office Chair", price: 150, quantity: 3 },
-    { category: "Clothing", item: "Leather Jacket", price: 200, quantity: 2 },
+    { category: "Home", item: "Microwave Oven", price: 300, quantity: 1 },
+    { category: "Books", item: "Office Chair", price: 150, quantity: 3 },
+    { category: "Books", item: "Leather Jacket", price: 200, quantity: 2 },
     { category: "Beauty", item: "Perfume", price: 100, quantity: 1 },
-    { category: "Sports", item: "Football", price: 50, quantity: 4 },
+    { category: "Electronics", item: "Football", price: 50, quantity: 4 },
     { category: "Books", item: "JavaScript Guide", price: 40, quantity: 5 },
-    { category: "Toys", item: "RC Car", price: 80, quantity: 2 },
-    { category: "Groceries", item: "Olive Oil", price: 20, quantity: 6 },
+    { category: "Home", item: "RC Car", price: 80, quantity: 2 },
+    { category: "Sports", item: "Olive Oil", price: 20, quantity: 6 },
 ];
+
+
+// console.log(products)
+
+// const data = products.reduce((table, items) => {
+
+//     if (table[items.quantity]) {
+//         table[items] = ++table[items.quantity]
+//     } else {
+//         table[items] = 1; //todo here initialize the value here
+//     }
+
+//     return table;
+// }, {})
+
+// console.log(data)
+
+
+const salesTables = products.reduce((table, sale) => {
+
+    console.log(table, ":", sale)
+    const { category, price, quantity } = sale
+
+    if (!table[category]) {
+
+        table[category] = {
+            totalRevenue: 0,
+            totalCost: 0,
+        }
+    }
+    table[category].totalRevenue += price * quantity;
+    table[category].totalCost += quantity;
+
+    return table;
+}, {})
+console.log(salesTables)
