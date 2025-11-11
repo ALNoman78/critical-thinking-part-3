e9bcb8b8d77ef6dd3022353157e21b5b7bb89155
ALNoman78 <abdullaalno078@gmail.com>
2025-11-11 21:39:10 +0600

    aggregation completed

--- twoaggreation.js
+++ twoaggreation.js
@@ -1,4 +1,69 @@
-//todo Data aggregation in array 
+// //todo Data aggregation in array
+
+// const surveyResponses = [
+//     "A",
+//     "C",
+//     "B",
+//     "A",
+//     "B",
+//     "B",
+//     "C",
+//     "A",
+//     "B",
+//     "D",
+//     "A",
+//     "C",
+//     "B",
+//     "A",
+//     "D",
+//     "B",
+//     "C",
+//     "A",
+//     "B",
+//     "D",
+//     "C",
+//     "A",
+//     "B",
+//     "A",
+//     "C",
+//     "B",
+//     "D",
+//     "A",
+//     "B",
+//     "C"
+// ];
+
+// // todo first check the empty object
+// // todo then I have to check the value is already initialize or not
+// // todo if the value is already exist then increment the value
+// // todo if the value is not initialize then I have to initialize the value
+
+
+// const count = surveyResponses.reduce((table, response) => {
+
+//     console.log(table, response)
+
+//     //todo readable code
+
+//     // if (table[response]) {
+//     //     table[response] = ++table[response]
+//     // } else {
+//     //     table[response] = 1
+//     // }
+
+//     // return table    //!! the best code is what you can understand and read also that's the best code for me
+
+//     // todo [ here is the one liner of this code ]
+
+//     table[response] = (table[response] || 0) + 1
+
+//     return table
+
+// }, {})
+
+// console.log(count);
+
+
 
 const surveyResponses = [
     "A",
@@ -33,32 +98,15 @@ const surveyResponses = [
     "C"
 ];
 
-// todo first check the empty object 
-// todo then I have to check the value is already initialize or not
-// todo if the value is already exist then increment the value 
-// todo if the value is not initialize then I have to initialize the value  
+const repeatData = surveyResponses.reduce((table, response) => {
 
-
-const count = surveyResponses.reduce((table, response) => {
-
-    console.log(table, response)
-
-    //todo readable code
-
-    // if (table[response]) {
-    //     table[response] = ++table[response]
-    // } else {
-    //     table[response] = 1
-    // }
-
-    // return table    //!! the best code is what you can understand and read also that's the best code for me
-
-    // todo [ here is the one liner of this code ]
-
-    table[response] = (table[response] || 0) + 1
+    if (table[response]) {
+        table[response] = ++table[response]
+    } else {
+        table[response] = 1
+    }
 
     return table
-
 }, {})
 
-console.log(count);
+console.log(repeatData);
